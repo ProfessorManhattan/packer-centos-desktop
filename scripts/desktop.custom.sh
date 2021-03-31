16731a8539142d881ce45ad395b4670a9555337f
Brian Zalewski <brian@megabyte.space>
2021-03-31 09:04:20 -0400

    Commit

--- scripts/desktop.custom.sh
+++ scripts/desktop.custom.sh
@@ -1,3 +1,10 @@
-#!/bin/sh -eux
+#!/bin/bash
 
-# TODO
+# Install GNOME-based GUI
+sudo yum groupinstall -y "Server with GUI"
+
+# Change systemd to start graphical mode
+sudo systemctl set-default graphical.target
+
+# Reboot system for changes to take effect
+sudo systemctl reboot
